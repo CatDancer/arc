1f2243319651f5797ecb3f4e0166bfd5751af3b1
Cat Dancer <cat@catdancer.ws>
2009-04-12 14:37:44 -0400

    allow Scheme values to pass through the Arc compiler

--- ac.scm
+++ ac.scm
@@ -69,7 +69,7 @@
         ; this line could be removed without changing semantics
         ((eq? (xcar (xcar s)) 'compose) (ac (decompose (cdar s) (cdr s)) env))
         ((pair? s) (ac-call (car s) (cdr s) env))
-        (#t (err "Bad object in expression" s))))
+        (#t s)))
 
 (define (literal? x)
   (or (boolean? x)
